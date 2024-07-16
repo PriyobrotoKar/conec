0170a61f259beb2a58149de1ed4e076e6eaaff14
Priyobroto Kar <priyobrotokar@gmail.com>
2024-07-16 09:52:26 +0530

    ci: refactored the staging workflow

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,5 +1,5 @@
 export const fetchFromApi = async (url: string) => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}api/` + url)
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/` + url)
   const result = await res.json()
   if (result.code === 500) {
     throw new Error(result.message)
